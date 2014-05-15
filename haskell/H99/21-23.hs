1f39a908c83fb5dcbe14f1328a3586cbf5e1a2ae
Dilbert de Romania <adriandp76@gmail.com>
2014-05-15 20:20:27 +0300

    p21-23

---
+++ haskell/H99/21-23.hs
@@ -0,0 +1,30 @@
+-- Problem 21: Insert an element at a given position into a list.
+
+module Problem21_23 (myInsert1, myInsert2) where
+import System.Random
+
+
+-- Problem 21
+myInsert1 :: [a] -> a -> Int -> [a]
+myInsert1 xs x n = take (n - 1) xs ++ [x] ++ drop (n - 1) xs
+
+
+myInsert2 :: [a] -> a -> Int -> [a]
+
+myInsert2 [] y _ = [y]
+myInsert2 xs y 1 = y:xs
+myInsert2 (x:xs) y n = x:myInsert2 xs y (n - 1)
+
+
+-- Problem 22: Create a list containing all integers within a given range.
+
+myRange :: Int -> Int -> [Int]
+myRange i j = [i..j]
+
+
+-- Problem 23: Extract a given number of randomly selected elements from a list.
+myRnd :: [a] -> Int -> IO [a]
+myRnd xs n = do
+	let len = length xs
+	g <- getStdGen
+	return (take n [xs !! k | k <- randomRs(0, len - 1) g])
